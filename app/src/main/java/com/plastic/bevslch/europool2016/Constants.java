ab0395c329b9e02554d9eda190fdfa8fe9f741ed
Oleksiy Martynov <3martynov@gmail.com>
2016-06-02 00:03:21 -0400

    constants added

---
+++ app/src/main/java/com/plastic/bevslch/europool2016/Constants.java
@@ -0,0 +1,10 @@
+package com.plastic.bevslch.europool2016;
+
+/**
+ * Created by Oleksiy on 6/1/2016.
+ */
+
+public class Constants {
+    public static final String BASE_URL ="http://www.nifegun.com";
+    public static final String contentTypeJson = "application/json";
+}
