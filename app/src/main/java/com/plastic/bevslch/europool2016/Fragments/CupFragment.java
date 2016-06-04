b5a13b516d082a5aef570115d319906ffba654db
omarbs <omar@plasticmobile.com>
2016-06-03 20:58:58 -0400

    Changes in the CupFragment layout and . Added some styles

--- app/src/main/java/com/plastic/bevslch/europool2016/Fragments/CupFragment.java
+++ app/src/main/java/com/plastic/bevslch/europool2016/Fragments/CupFragment.java
@@ -2,6 +2,9 @@ package com.plastic.bevslch.europool2016.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +16,13 @@ import com.plastic.bevslch.europool2016.R;
  */
 
 public class CupFragment extends Fragment {
+
+    private static final String TAG = CupFragment.class.getSimpleName();
+
+    private View fragmentView;
+    private SwipeRefreshLayout refreshLayout;
+    private RecyclerView upcomingList, completedList;
+
     public CupFragment() {
         // Required empty public constructor
     }
@@ -25,7 +35,47 @@ public class CupFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cup, container, false);
+
+        fragmentView = inflater.inflate(R.layout.fragment_cup, container, false);
+
+        initView();
+        initListeners();
+        configView();
+
+        return fragmentView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        updateLists();
+    }
+
+    private void initView() {
+        refreshLayout = (SwipeRefreshLayout) fragmentView.findViewById(R.id.cup_refresh);
+        upcomingList = (RecyclerView) fragmentView.findViewById(R.id.cup_upcoming_list);
+        completedList = (RecyclerView) fragmentView.findViewById(R.id.cup_completed_list);
+    }
+
+    private void initListeners() {
+
+        refreshLayout.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        updateLists();
+                    }
+                }
+        );
+    }
+
+    private void configView() {
+        upcomingList.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        completedList.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+    }
+
+    private void updateLists() {
+        refreshLayout.setRefreshing(false);
+
+
     }
 }
